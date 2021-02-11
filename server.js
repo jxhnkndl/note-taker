2e799d8ae381180e5913e692399e277462f7b211
jxhnkndl <jkroyston@gmail.com>
2021-02-11 16:17:32 -0500

    Add middleware to serve public folder statically and fix conflict between * and /notes routes

--- server.js
+++ server.js
@@ -8,18 +8,18 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware - Helps parse data
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
-// ROUTE (HTML): index/root/*
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-});
 
 // ROUTE (HTML): /notes
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+// ROUTE (HTML): index/root/*
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // Start listening
